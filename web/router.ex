de5abdbae4a127dc689012733e2597c876f8c0a7
Alberto Flores <Alberto@Flores.cf>
2017-02-11 14:24:21 -0700

    Added the resources created to the router

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule PhonebookElixir.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/agenda", ContactController
   end
 
   # Other scopes may use custom stacks.
